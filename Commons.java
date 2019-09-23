4086aa2caf95060990580bb8adc6f7429b2358d6
Jan Bodnar <jan.bodnar@gmail.com>
2019-09-23 20:39:22 +0200

    Create Commons.java

---
+++ Commons.java
@@ -0,0 +1,19 @@
+package com.zetcode;
+
+public interface Commons {
+
+    public static final int BOARD_WIDTH = 358;
+    public static final int BOARD_HEIGHT = 350;
+    public static final int GROUND = 290;
+    public static final int BOMB_HEIGHT = 5;
+    public static final int ALIEN_HEIGHT = 12;
+    public static final int ALIEN_WIDTH = 12;
+    public static final int BORDER_RIGHT = 30;
+    public static final int BORDER_LEFT = 5;
+    public static final int GO_DOWN = 15;
+    public static final int NUMBER_OF_ALIENS_TO_DESTROY = 24;
+    public static final int CHANCE = 5;
+    public static final int DELAY = 17;
+    public static final int PLAYER_WIDTH = 15;
+    public static final int PLAYER_HEIGHT = 10;
+}
