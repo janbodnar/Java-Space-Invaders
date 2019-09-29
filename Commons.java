347909d7e73a4a1fa849c1729f1d3c4e0957efc9
Jan Bodnar <jan.bodnar@gmail.com>
2019-09-29 21:52:21 +0200

    Update Commons.java

--- Commons.java
+++ Commons.java
@@ -2,18 +2,23 @@ package com.zetcode;
 
 public interface Commons {
 
-    public static final int BOARD_WIDTH = 358;
-    public static final int BOARD_HEIGHT = 350;
-    public static final int GROUND = 290;
-    public static final int BOMB_HEIGHT = 5;
-    public static final int ALIEN_HEIGHT = 12;
-    public static final int ALIEN_WIDTH = 12;
-    public static final int BORDER_RIGHT = 30;
-    public static final int BORDER_LEFT = 5;
-    public static final int GO_DOWN = 15;
-    public static final int NUMBER_OF_ALIENS_TO_DESTROY = 24;
-    public static final int CHANCE = 5;
-    public static final int DELAY = 17;
-    public static final int PLAYER_WIDTH = 15;
-    public static final int PLAYER_HEIGHT = 10;
+    int BOARD_WIDTH = 358;
+    int BOARD_HEIGHT = 350;
+    int BORDER_RIGHT = 30;
+    int BORDER_LEFT = 5;
+
+    int GROUND = 290;
+    int BOMB_HEIGHT = 5;
+
+    int ALIEN_HEIGHT = 12;
+    int ALIEN_WIDTH = 12;
+    int ALIEN_INIT_X = 150;
+    int ALIEN_INIT_Y = 5;
+
+    int GO_DOWN = 15;
+    int NUMBER_OF_ALIENS_TO_DESTROY = 24;
+    int CHANCE = 5;
+    int DELAY = 17;
+    int PLAYER_WIDTH = 15;
+    int PLAYER_HEIGHT = 10;
 }
