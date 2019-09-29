e1bf2484e30917c696a928b55a252794ddf561b8
Jan Bodnar <jan.bodnar@gmail.com>
2019-09-29 22:05:47 +0200

    Update Player.java

--- Player.java
+++ Player.java
@@ -1,15 +1,12 @@
-package com.zetcode;
+package com.zetcode.sprite;
 
-import java.awt.event.KeyEvent;
+import com.zetcode.Commons;
 
 import javax.swing.ImageIcon;
+import java.awt.event.KeyEvent;
 
-public class Player extends Sprite implements Commons {
+public class Player extends Sprite {
 
-    private final int START_Y = 280;
-    private final int START_X = 270;
-
-    private final String playerImg = "src/images/player.png";
     private int width;
 
     public Player() {
@@ -18,51 +15,61 @@ public class Player extends Sprite implements Commons {
     }
 
     private void initPlayer() {
-        
-        ImageIcon ii = new ImageIcon(playerImg);
+
+        var playerImg = "src/images/player.png";
+        var ii = new ImageIcon(playerImg);
 
         width = ii.getImage().getWidth(null);
-
         setImage(ii.getImage());
+
+        int START_X = 270;
         setX(START_X);
+
+        int START_Y = 280;
         setY(START_Y);
     }
 
     public void act() {
-        
+
         x += dx;
-        
+
         if (x <= 2) {
+
             x = 2;
         }
-        
-        if (x >= BOARD_WIDTH - 2 * width) {
-            x = BOARD_WIDTH - 2 * width;
+
+        if (x >= Commons.BOARD_WIDTH - 2 * width) {
+
+            x = Commons.BOARD_WIDTH - 2 * width;
         }
     }
 
     public void keyPressed(KeyEvent e) {
-        
+
         int key = e.getKeyCode();
 
         if (key == KeyEvent.VK_LEFT) {
+
             dx = -2;
         }
 
         if (key == KeyEvent.VK_RIGHT) {
+
             dx = 2;
         }
     }
 
     public void keyReleased(KeyEvent e) {
-        
+
         int key = e.getKeyCode();
 
         if (key == KeyEvent.VK_LEFT) {
+
             dx = 0;
         }
 
         if (key == KeyEvent.VK_RIGHT) {
+
             dx = 0;
         }
     }
