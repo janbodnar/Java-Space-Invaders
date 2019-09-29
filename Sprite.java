daaa11d111c1d25509acc7d26461a111feee199f
Jan Bodnar <jan.bodnar@gmail.com>
2019-09-29 22:01:03 +0200

    Update Sprite.java

--- Sprite.java
+++ Sprite.java
@@ -1,4 +1,4 @@
-package com.zetcode;
+package com.zetcode.sprite;
 
 import java.awt.Image;
 
@@ -6,67 +6,69 @@ public class Sprite {
 
     private boolean visible;
     private Image image;
-    protected int x;
-    protected int y;
-    protected boolean dying;
-    protected int dx;
+    private boolean dying;
+
+    int x;
+    int y;
+    int dx;
 
     public Sprite() {
-        
+
         visible = true;
     }
 
     public void die() {
-        
+
         visible = false;
     }
 
     public boolean isVisible() {
-        
+
         return visible;
     }
 
     protected void setVisible(boolean visible) {
+
         this.visible = visible;
     }
 
     public void setImage(Image image) {
-        
+
         this.image = image;
     }
 
     public Image getImage() {
-        
+
         return image;
     }
 
     public void setX(int x) {
-        
+
         this.x = x;
     }
 
     public void setY(int y) {
-        
+
         this.y = y;
     }
 
     public int getY() {
-        
+
         return y;
     }
 
     public int getX() {
-        
+
         return x;
     }
 
     public void setDying(boolean dying) {
-        
+
         this.dying = dying;
     }
 
     public boolean isDying() {
-        
+
         return this.dying;
     }
 }
