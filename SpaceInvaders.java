4d7dd3dec648f986c2c50a6b945479c749755c14
Jan Bodnar <jan.bodnar@gmail.com>
2019-09-29 21:49:38 +0200

    Update SpaceInvaders.java

--- SpaceInvaders.java
+++ SpaceInvaders.java
@@ -3,7 +3,7 @@ package com.zetcode;
 import java.awt.EventQueue;
 import javax.swing.JFrame;
 
-public class SpaceInvaders extends JFrame implements Commons {
+public class SpaceInvaders extends JFrame  {
 
     public SpaceInvaders() {
 
@@ -13,17 +13,20 @@ public class SpaceInvaders extends JFrame implements Commons {
     private void initUI() {
 
         add(new Board());
+
         setTitle("Space Invaders");
+        setSize(Commons.BOARD_WIDTH, Commons.BOARD_HEIGHT);
+
         setDefaultCloseOperation(EXIT_ON_CLOSE);
-        setSize(BOARD_WIDTH, BOARD_HEIGHT);
-        setLocationRelativeTo(null);
         setResizable(false);
+        setLocationRelativeTo(null);
     }
 
     public static void main(String[] args) {
-        
+
         EventQueue.invokeLater(() -> {
-            SpaceInvaders ex = new SpaceInvaders();
+
+            var ex = new SpaceInvaders();
             ex.setVisible(true);
         });
     }
